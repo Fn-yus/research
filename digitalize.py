b34acef15bb0ffdc6612e7a041a9964343def223
yusei <yusei42108991@gmail.com>
2019-09-02 22:41:43 +0900

    fix digitalize.py

--- digitalize.py
+++ digitalize.py
@@ -5,6 +5,6 @@ def main(needle, scale):
     scale_upper_index = min(scale_upper_list)
     scale_lower_list, = np.where(needle >= scale)
     scale_lower_index = max(scale_lower_list)
-    needle_percentage = needle/(scale[scale_upper_index] - scale[scale_lower_index])
+    needle_percentage = (needle - scale[scale_lower_index])/(scale[scale_upper_index] - scale[scale_lower_index])
     needle_position = (scale_lower_index - 3) + needle_percentage
     return needle_position
