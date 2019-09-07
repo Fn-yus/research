4f6cb2cac86ff252c5781683ea5d4e52b7e656a3
yusei <yusei42108991@gmail.com>
2019-09-07 23:34:21 +0900

    change main.py for operating many pictures.

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import cv2
 import numpy as np
 import glob
+import os
 import configparser
 
-#必要に応じてpathを書き換えること
-
 def trimming(fname):
-    
+    if "Long-needle" in fname:
+        img = cv2.imread(fname)
+        height = img.shape[0]
+        width = img.shape[1]
+        img_trimmed = cv2.rotate(img[192:288, 81:549], cv2.ROTATE_180)
+        new_fname = os.path.basename(fname)
+        return img_trimmed
+    else:
+        pass
 
-def extract_needle():
-    img = cv2.imread("pictures/sample_1.jpg") #画像読み込み
-
+def extract_needle(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #BGRからHSVに変換
     red_lower = np.array([0, 86, 86]) #下限
     red_upper = np.array([30, 255, 255]) #上限
@@ -18,26 +23,26 @@ def extract_needle():
     img_mask = cv2.inRange(hsv, red_lower, red_upper) #範囲を指定してマスク画像作成
     img_needle = cv2.bitwise_and(img, img, mask=img_mask) #元画像とマスク画像の共通部分を抽出
 
-    cv2.imwrite("pictures/sample_1_needle.jpg", img_needle) #画像書き出し
+    return img_needle
 
-def hough_lines():
-    img = cv2.imread('pictures/sample_1.jpg')
+def hough_lines(img, img_needle):
+    #img = cv2.imread('pictures/sample_1.jpg')
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #グレースケール化
     img_gray_denoised = cv2.fastNlMeansDenoising(img_gray)
     img_thresh = cv2.adaptiveThreshold(img_gray_denoised,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,17,5)
     img_canny = cv2.Canny(img_gray_denoised, 50, 150)    
     img_thresh2 = cv2.bitwise_not(img_thresh)
     img_canny2 = cv2.bitwise_not(img_canny)
-    cv2.imwrite('pictures/sample_1_canny.jpg', img_canny2)
+    #cv2.imwrite('pictures/sample_1_canny.jpg', img_canny2)
 
-    img_needle = cv2.imread('pictures/sample_1_needle.jpg')
+    #img_needle = cv2.imread('pictures/sample_1_needle.jpg')
     img_needle_gray = cv2.cvtColor(img_needle, cv2.COLOR_BGR2GRAY)
     ret, img_needle_thresh = cv2.threshold(img_needle_gray, 50, 255, cv2.THRESH_BINARY_INV)
     img_needle_thresh2 = cv2.bitwise_not(img_needle_thresh)
     img_needle_thresh_diff = cv2.subtract(img_needle_thresh, img_thresh)
     img_needle_canny = cv2.Canny(img_needle_gray, 50, 150)
     img_needle_canny2 = cv2.bitwise_not(img_needle_canny)
-    cv2.imwrite('pictures/sample_1_needle_canny.jpg', img_needle_canny2)
+    #cv2.imwrite('pictures/sample_1_needle_canny.jpg', img_needle_canny2)
 
     needle_lines = cv2.HoughLines(img_needle_thresh2,1,np.pi,20)
     #print(needle_lines)
@@ -75,12 +80,12 @@ def hough_lines():
     #cv2.imshow('sample_1_needle_thresh.jpg', img_needle_thresh)
     cv2.imshow('sample_1_needle_thresh_diff.jpg', img_needle_thresh_diff)
 
-    return {"needle_line":needle_line, "img_lines":img_lines, "line_list":line_list}
+    return {"needle_line":needle_line, "img_lines":img_lines, "line_list":line_list, "img_canny":img_canny2, "img_needle_canny":img_needle_canny2}
 
-def identify_scale():
-    img_needle = cv2.imread('pictures/sample_1_needle_canny.jpg')
-    img_needle_gray = cv2.cvtColor(img_needle, cv2.COLOR_BGR2GRAY) #画像を再度読み込んでいるので、再度グレースケール化
-    ret, img_needle_thresh = cv2.threshold(img_needle_gray, 127, 255, cv2.THRESH_BINARY)
+def identify_scale(img_canny, img_needle_canny):
+    #img_needle = cv2.imread('pictures/sample_1_needle_canny.jpg')
+    #img_needle_gray = cv2.cvtColor(img_needle_canny, cv2.COLOR_BGR2GRAY) #画像を再度読み込んでいるので、再度グレースケール化
+    ret, img_needle_thresh = cv2.threshold(img_needle_canny, 127, 255, cv2.THRESH_BINARY)
 
     needle = None
     for row in img_needle_thresh:
@@ -91,9 +96,9 @@ def identify_scale():
         else:
             continue
 
-    img = np.array(cv2.imread('pictures/sample_1_canny.jpg'))
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-    ret, img_thresh = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
+    #img = np.array(cv2.imread('pictures/sample_1_canny.jpg'))
+    #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    ret, img_thresh = cv2.threshold(img_canny, 127, 255, cv2.THRESH_BINARY)
 
     scale_list = None
     for row in img_thresh:
@@ -123,28 +128,29 @@ def digitalize(needle, scale):
     return needle_position
 
 if __name__ == "__main__":
-    #config = configparser.ConfigParser()
-    #config.read("config/config.ini")
-    files = glob.glob("C:\\Users\\Yusei\\D58-pictures\\Long-needle\\*.jpg") 
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
+    files = glob.glob('C:\\Users\\Yusei\\D58-pictures\\Long-needle\\*.jpg') 
     
     for fname in files:
         img = trimming(fname)
-        hough_lines = hough_lines()
-        identify_scale = identify_scale()
+        img_needle = extract_needle(img)
+        houghlines = hough_lines(img, img_needle)
+        identifyscale = identify_scale(houghlines["img_canny"], houghlines["img_needle_canny"])
         print("======================")
         print("Houghlines\n")
-        print("・針の位置：" + str(hough_lines["needle_line"]))
-        print("・直線の検出数：" + str(len(hough_lines["img_lines"])))
-        print("・調整後の直線数：" + str(len(hough_lines["line_list"])))
-        print("・調整後の目盛り座標：" + str(sorted(hough_lines["line_list"])))
-        print("・目盛り幅：" + str(np.diff(sorted(hough_lines["line_list"]), n = 1)))
-        print("・針の座標：" + str(digitalize(hough_lines["needle_line"], sorted(hough_lines["line_list"]))))
+        print("・針の位置：" + str(houghlines["needle_line"]))
+        print("・直線の検出数：" + str(len(houghlines["img_lines"])))
+        print("・調整後の直線数：" + str(len(houghlines["line_list"])))
+        print("・調整後の目盛り座標：" + str(sorted(houghlines["line_list"])))
+        print("・目盛り幅：" + str(np.diff(sorted(houghlines["line_list"]), n = 1)))
+        print("・針の座標：" + str(digitalize(houghlines["needle_line"], sorted(houghlines["line_list"]))))
         print("======================")
         print("Identify Bold Scale Along Rows\n")
-        print("・針の位置：" + str(identify_scale["needle"]))
-        print("・目盛り座標：" + str(identify_scale["scales"]))
-        print("・目盛り幅：" + str(np.diff(identify_scale["scales"], n = 1)))
-        print("・針の座標：" + str(digitalize(identify_scale["needle"], identify_scale["scales"])))
+        print("・針の位置：" + str(identifyscale["needle"]))
+        print("・目盛り座標：" + str(identifyscale["scales"]))
+        print("・目盛り幅：" + str(np.diff(identifyscale["scales"], n = 1)))
+        print("・針の座標：" + str(digitalize(identifyscale["needle"], identifyscale["scales"])))
         print("======================")
 
     cv2.waitKey(0)
