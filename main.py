b18ba406fa82e1b99b7d12ecdbd31595d98ec339
yusei <yusei42108991@gmail.com>
2019-09-21 16:33:42 +0900

    Add some conditions.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import glob
 import os
 import configparser
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
+from tqdm import tqdm
 
 def trimming(fname):
     if "Long-needle" in fname:
@@ -11,8 +12,8 @@ def trimming(fname):
         height = img.shape[0]
         width = img.shape[1]
         img_trimmed = cv2.rotate(img[192:288, 81:549], cv2.ROTATE_180)
-        new_fname = os.path.basename(fname)
-        return img_trimmed
+        cv2.imwrite('results/pictures/img.jpg', img_trimmed)
+        return img_trimmed     
     else:
         pass
 
@@ -43,7 +44,7 @@ def hough_lines(img, img_needle):
     img_needle_thresh_diff = cv2.subtract(img_needle_thresh, img_thresh)
     img_needle_canny = cv2.Canny(img_needle_gray, 50, 150)
     img_needle_canny2 = cv2.bitwise_not(img_needle_canny)
-    #cv2.imwrite('pictures/sample_1_needle_canny.jpg', img_needle_canny2)
+    #cv2.imwrite('pictures/sample_1_needle_canny.jpg', img_needle_thresh_diff)
 
     needle_lines = cv2.HoughLines(img_needle_thresh2,1,np.pi,20)
     #print(needle_lines)
@@ -83,33 +84,61 @@ def hough_lines(img, img_needle):
 
     return {"needle_line":needle_line, "img_lines":img_lines, "line_list":line_list, "img_canny":img_canny2, "img_needle_canny":img_needle_canny2}
 
-def identify_scale(img_canny, img_needle_canny):
+def identify_scale(img_canny, img_needle_canny, new_fname):
     #img_needle = cv2.imread('pictures/sample_1_needle_canny.jpg')
     #img_needle_gray = cv2.cvtColor(img_needle_canny, cv2.COLOR_BGR2GRAY) #画像を再度読み込んでいるので、再度グレースケール化
     ret, img_needle_thresh = cv2.threshold(img_needle_canny, 127, 255, cv2.THRESH_BINARY)
 
-    needle = None
+    needle = []
     for row in img_needle_thresh:
         needle_list, = np.where(row == 0)
-        if len(needle_list) == 2 and np.diff(needle_list) > 5:
-            needle = np.mean(needle_list)
-            break 
+        if len(needle_list) == 2:
+            if needle_list[0] >= 450 and np.diff(needle_list) >= 3: #端の場合
+                needle = np.mean(needle_list)
+                break
+            elif np.diff(needle_list) >= 5:
+                needle = np.mean(needle_list)
+                break
         else:
             continue
+
+    if needle == []:
+        for row in img_needle_thresh:
+            needle_list, = np.where(row == 0) #色が黒の箇所を抽出
+            print(len(needle_list))
+            print(needle_list)
+        print(new_fname)
+    else:
+        pass
+
     
-    cv2.line(img_canny, (Decimal(str(needle)).quantize(Decimal("0")), 1000), (Decimal(str(needle)).quantize(Decimal("0")), -1000), (0, 0, 255), 1)
     #img = np.array(cv2.imread('pictures/sample_1_canny.jpg'))
     #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     ret, img_thresh = cv2.threshold(img_canny, 127, 255, cv2.THRESH_BINARY)
+    img_thresh_diff = cv2.bitwise_not(cv2.subtract(img_needle_thresh, img_thresh))
+    cv2.imwrite('results/pictures/img_needle_thresh.jpg', img_needle_thresh)
+    cv2.imwrite('results/pictures/img_thresh.jpg', img_thresh)
+    cv2.imwrite('results/pictures/img_thresh_diff.jpg', img_thresh_diff)
+    print(needle)
 
-    scale_list = None
-    for row in img_thresh:
-        black_list, = np.where(row == 0)
+    scale_list = []
+    for row in img_thresh_diff:
+        black_list, = np.where(row == 0) #色が黒の箇所を抽出
         if len(black_list) == 14 and min(np.diff(black_list[0::2])) > 50: #目盛りの縁が14個かつ目盛り間のピクセル距離が50以上
             scale_list = black_list
             break
         else:
             continue
+    
+    #うまくいけばここはpass
+    if scale_list == []:
+        for row in img_thresh_diff:
+            black_list, = np.where(row == 0) #色が黒の箇所を抽出
+            print(len(black_list))
+            print(black_list)
+        print(new_fname)
+    else:
+        pass
 
     scale_list_splited = np.split(np.array(scale_list), 7)
 
@@ -118,10 +147,11 @@ def identify_scale(img_canny, img_needle_canny):
         scale = np.mean(row)
         scales.append(scale)
     
+    cv2.line(img_canny, (Decimal(str(needle)).quantize(Decimal("0")), 1000), (Decimal(str(needle)).quantize(Decimal("0")), -1000), (0, 0, 255), 1)
+
     for i in scales:
         cv2.line(img_canny, (Decimal(str(i)).quantize(Decimal("0")), 1000), (Decimal(str(i)).quantize(Decimal("0")), -1000), (0, 255, 0), 1)
 
-    cv2.imwrite('samples/identify_scale' + fname, img_canny)
 
     return {"needle":needle, "scales":scales}
 
@@ -137,13 +167,16 @@ def digitalize(needle, scale):
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read("config/config.ini")
-    files = glob.glob('C:\\Users\\Yusei\\D58-pictures\\Long-needle\\*.jpg') 
+    files = glob.glob('C:\\Users\\Yusei\\D58-pictures\\Long-needle\\data\\*.jpg') 
     
-    for fname in files:
+    for fname in tqdm(files):
+        new_fname, ext = os.path.splitext(os.path.basename(fname))
         img = trimming(fname)
         img_needle = extract_needle(img)
         houghlines = hough_lines(img, img_needle)
-        identifyscale = identify_scale(houghlines["img_canny"], houghlines["img_needle_canny"])
+        identifyscale = identify_scale(houghlines["img_canny"], houghlines["img_needle_canny"], new_fname)
+        print("======================")
+        print(new_fname)
         print("======================")
         print("Houghlines\n")
         print("・針の位置：" + str(houghlines["needle_line"]))
