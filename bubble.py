577a5a1ca663fc05001d84a753c894a3ccf08f2b
Yusei Okada <yusei42108991@gmail.com>
2019-10-24 15:45:45 +0900

    create bubble.py

---
+++ bubble.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from decimal import *
+from datetime import datetime, timedelta
+from time import sleep
+import glob
+import os
+import configparser
+import csv
+from tqdm import tqdm
+
+def trimming():
+    
