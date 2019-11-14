f6b975712ea16a17397ca1c91b044189d1018c16
Yusei Okada <yusei42108991@gmail.com>
2019-11-14 18:29:41 +0900

    add function identify_bubble.

--- bubble.py
+++ bubble.py
@@ -52,7 +52,7 @@ def identify_scale(img, fname):
                 if min(np.diff(black_list[0::2])) > 20: #目盛り間の幅が20ピクセル以上
                     if black_list[8] - black_list[7] > 150: #左側と右側の目盛りのギャップを確実に
                         scale_list.append(black_list)
-                        print(black_list)
+                        # print(black_list)
 
     if len(scale_list) == 0:
         for row in img_thresh:
@@ -67,7 +67,7 @@ def identify_scale(img, fname):
     scale_list_splited = np.split(np.array(scale_list), 8)  #目盛りの左右の線ごとにまとめる
     scales = [np.mean(row) for row in scale_list_splited]   #目盛りの左右の線の平均を取り、scalesにappend
 
-    print(scales)
+    # print(scales)
 
     for i in scales:
         cv2.line(img, (Decimal(str(i)).quantize(Decimal("0")), 1000), (Decimal(str(i)).quantize(Decimal("0")), -1000), (0, 255, 0), 1)
@@ -75,7 +75,26 @@ def identify_scale(img, fname):
     cv2.imwrite('img_thresh_canny2.jpg', img_thresh_canny2)
     cv2.imwrite('img_lined.jpg'.format(fname), img)
 
-    return scales
+    return scales, img_gray_denoised
+
+def identify_bubble(fname, img):
+    ret, img_bubble_thresh = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+    img_bubble_thresh_canny = cv2.Canny(img_bubble_thresh, 0, 100)
+    img_bubble_canny = cv2.Canny(img, 450, 550)
+    cv2.imwrite('img_bubble_thresh.jpg', img_bubble_thresh)
+    cv2.imwrite('img_bubble_thresh_canny.jpg', img_bubble_thresh_canny)
+    cv2.imwrite('img_bubble_canny.jpg', img_bubble_canny)
+
+    contours, hierarchy = cv2.findContours(img_bubble_canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img_contour = cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+    print(contours)
+    #for cnt in contours:
+    #    ellipse = cv2.fitEllipse(cnt)
+    #    img_bubble_canny = cv2.ellipse(img, ellipse, (0,255,0), 2)
+    #    cv2.imwrite('img_ellipse.jpg', img_bubble_canny)
+    cv2.imwrite('img_bubble_canny_after.jpg', img_contour)
+
+
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
@@ -86,7 +105,9 @@ if __name__ == "__main__":
     for fname in target_files: #決め打ち
         print(fname)
         img = trimming(fname)
-        scales = identify_scale(img, fname)
+        identify_scale_list = identify_scale(img, fname)
+        scales = identify_scale_list[0]
+        identify_bubble(fname, identify_scale_list[1])
         csv_list = sum([[fname], scales], []) #平坦化している
         csv_lists.append(csv_list)
 
